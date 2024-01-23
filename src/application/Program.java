8d43edf7155e8de00d44730a48ce29c3f715535b
pedro-batista <peddrobatista2022@gmail.com>
2024-01-23 16:06:58 -0300

    Primeiro commit...

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		ConcretePrinter p = new ConcretePrinter("1080");
+		p.processDoc("My letter");
+		p.print("My letter");
+		System.out.println();
+		
+		ConcreteScanner s = new ConcreteScanner("2003");
+		s.processDoc("My email");
+		System.out.println("Scanner result: " + s.scan());
+		System.out.println();
+		
+		ComboDevice c  = new ComboDevice("2081");
+		c.processDoc("My dissertation");
+		c.print("My dissertation");
+		System.out.println("Scan result: " + c.scan());
+
+	}
+
+}
